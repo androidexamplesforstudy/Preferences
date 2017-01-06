84acafc027fb4c2f42af1c17c30c073843db02c6
AdamK <Adamkubarek@gmail.com>
2017-01-06 23:58:52 +0100

    everything done

--- app/src/main/java/example/com/preferences/MainActivity.java
+++ app/src/main/java/example/com/preferences/MainActivity.java
@@ -1,7 +1,10 @@
 package example.com.preferences;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -14,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     TextView textView2;
     TextView textView3;
 
+    Typeface fontNumber1;
+    Typeface fontNumber2;
+
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +32,71 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         textView2 = (TextView) findViewById(R.id.textView2);
         textView3 = (TextView) findViewById(R.id.textView3);
+
+        fontNumber1 = Typeface.createFromAsset(getAssets(), "fonts/theme1.ttf");
+        fontNumber2 = Typeface.createFromAsset(getAssets(), "fonts/theme2.ttf");
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        specifyTheFontForText1();
+        specifyTheFontForText2();
+        specifyTheFontForText3();
+    }
+
+    public void specifyTheFontForText1 () {
+        boolean fontNumber1Value;
+        boolean fontNumber2Value;
+        String fontSize = sharedPreferences.getString("FONT_SIZE_1", "10");
+
+        fontNumber1Value = sharedPreferences.getBoolean("CHECKBOX_FONT_NR1_TEXT1", false);
+        fontNumber2Value = sharedPreferences.getBoolean("CHECKBOX_FONT_NR2_TEXT1", false);
+
+        if (fontNumber1Value) {
+            textView.setTypeface(fontNumber1);
+
+        } else if (fontNumber2Value) {
+            textView.setTypeface(fontNumber2);
+        }
+
+        textView.setTextSize(Integer.valueOf(fontSize));
+    }
+
+    public void specifyTheFontForText2 () {
+        boolean fontNumber1Value;
+        boolean fontNumber2Value;
+        String fontSize = sharedPreferences.getString("FONT_SIZE_2", "10");
+
+        fontNumber1Value = sharedPreferences.getBoolean("CHECKBOX_FONT_NR1_TEXT2", false);
+        fontNumber2Value = sharedPreferences.getBoolean("CHECKBOX_FONT_NR2_TEXT2", false);
+
+        if (fontNumber1Value) {
+            textView2.setTypeface(fontNumber1);
+
+        } else if (fontNumber2Value) {
+            textView2.setTypeface(fontNumber2);
+        }
+        textView2.setTextSize(Integer.valueOf(fontSize));
+    }
+
+    public void specifyTheFontForText3 () {
+        boolean fontNumber1Value;
+        boolean fontNumber2Value;
+        String fontSize = sharedPreferences.getString("FONT_SIZE_3", "10");
+
+        fontNumber1Value = sharedPreferences.getBoolean("CHECKBOX_FONT_NR1_TEXT3", false);
+        fontNumber2Value = sharedPreferences.getBoolean("CHECKBOX_FONT_NR2_TEXT3", false);
+
+        if (fontNumber1Value) {
+            textView3.setTypeface(fontNumber1);
+
+        } else if (fontNumber2Value) {
+            textView3.setTypeface(fontNumber2);
+        }
+        textView3.setTextSize(Integer.valueOf(fontSize));
     }
 
     @Override
